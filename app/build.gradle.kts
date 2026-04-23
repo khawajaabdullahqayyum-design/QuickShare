1fef2fb145ad844e63229048be7df0dcc48f91ba
khawajaabdullahqayyum-design <khawaja.abdullah.qayyum@gmail.com>
2026-04-23 10:29:48 +0500

    Add SwipeRefreshLayout dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 
     // Jetpack
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
